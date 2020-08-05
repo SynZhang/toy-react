7693d82db5d0c6ec9e4ff4fb4314789b3b8054ef
Syn Zhang <zeyanzhang.china@gmail.com>
2020-08-06 00:41:25 +0800

    update: support state and re-render

--- index.js
+++ index.js
@@ -1,22 +1,51 @@
 import { ToyReact, Component } from './ToyReact'
 
-class MyComponent extends Component {
+class Square extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: null,
+    }
+  }
+  render() {
+    return (
+      <button className='square' onClick={() => this.setState({ value: 'X' })}>
+        {this.state.value || ''}
+      </button>
+    )
+  }
+}
+
+class Board extends Component {
+  renderSquare(i) {
+    return <Square value={i} />
+  }
+
   render() {
     return (
       <div>
-        <span>Hello </span>
-        <span>ToyReact</span>
-        <span>!</span>
-        {this.children}
+        <div className='board-row'>
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className='board-row'>
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className='board-row'>
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
       </div>
     )
   }
 }
 
 const App = (
-  <MyComponent>
-    <div>Hello Child.</div>
-  </MyComponent>
+  <Board />
 )
 
 ToyReact.render(App, document.body)
